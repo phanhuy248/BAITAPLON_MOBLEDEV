347c4e4acf8b2be14de4c091690a7a57cea7d4c1
phanhuy248 <phanquanghuy248@gmail.com>
2025-04-14 20:11:11 +0700

    Xoa

--- app/src/main/java/com/neatroots/instagramclone/utils/Constant.kt
+++
@@ -1,8 +0,0 @@
-package com.neatroots.instagramclone.utils
-
-const val USER_NODE = "User"
-const val USER_PROFILE_FOLDER = "Profile"
-const val POST_FOLDER = "PostImages"
-const val REEL_FOLDER = "ReelVideos"
-const val POST = "Post"
-const val REEL = "Reel"
